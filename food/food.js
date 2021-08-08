96d946569c3f080a7d6ce5b391066252e0514126
Abdelrahman059 <82518895+Abdelrahman059@users.noreply.github.com>
2021-08-09 00:42:47 +0300

    Update Food page
    Repairing Local Storage Done
    related #46

--- food/food.js
+++ food/food.js
@@ -6,51 +6,12 @@ button.addEventListener("click", function () {
   location.reload();
 });
 
-if(localStorage.getItem(1) != null)
-{
-fetch(
-  `https://www.themealdb.com/api/json/v1/1/search.php?s=${localStorage.getItem(1)}`
-)
-  .then((response) => {
-    if (response.status !== 200) {
-      console.log(
-        `Looks like there was a problem. Status Code: ${response.status}`
-      );
-    } else {
-      // Examine the text in the response
-      return response.json();
-    }
-  })
-  .then((data) => {
-    let dat = data.meals;
-    for (element of dat) {
-      // Create a div with a card class
-      const link= document.createElement("a");
-      link.setAttribute("href","../selectedProduct/selectedProduct.html");
-      const Meal = document.createElement("div");
-      link.appendChild(Meal);
-      Meal.setAttribute("class", "FirstOpject");
-      Container.appendChild(link);
-      const imgMeal = document.createElement("img");
-      imgMeal.setAttribute("class", "imgOfOpject");
-
-      Meal.appendChild(imgMeal);
-
-      imgMeal.setAttribute("src", `${element.strMealThumb}`);
-      const favoriteDrink = document.createElement("div");
-      favoriteDrink.setAttribute("class", "favorite");
-
-      Meal.appendChild(favoriteDrink);
-
-      const favIcon = document.createElement("i");
-      favIcon.setAttribute("class", "ri-heart-line");
-      favoriteDrink.appendChild(favIcon);
-    }
-  });
-}
-else{
+if (localStorage.getItem(1) != null) {
   fetch(
-    `https://www.themealdb.com/api/json/v1/1/search.php?s=` )
+    `https://www.themealdb.com/api/json/v1/1/search.php?s=${localStorage.getItem(
+      1
+    )}`
+  )
     .then((response) => {
       if (response.status !== 200) {
         console.log(
@@ -65,27 +26,72 @@ else{
       let dat = data.meals;
       for (element of dat) {
         // Create a div with a card class
-        const link= document.createElement("a");
-      link.setAttribute("href","../selectedProduct/selectedProduct.html");
+        const link = document.createElement("a");
+        link.setAttribute("href", "../selectedProduct/selectedProduct.html");
         const Meal = document.createElement("div");
         link.appendChild(Meal);
         Meal.setAttribute("class", "FirstOpject");
         Container.appendChild(link);
         const imgMeal = document.createElement("img");
         imgMeal.setAttribute("class", "imgOfOpject");
-  
+
         Meal.appendChild(imgMeal);
-  
+
         imgMeal.setAttribute("src", `${element.strMealThumb}`);
         const favoriteDrink = document.createElement("div");
         favoriteDrink.setAttribute("class", "favorite");
-  
+
         Meal.appendChild(favoriteDrink);
-  
+
         const favIcon = document.createElement("i");
         favIcon.setAttribute("class", "ri-heart-line");
         favoriteDrink.appendChild(favIcon);
       }
     });
+} else {
+  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=`)
+    .then((response) => {
+      if (response.status !== 200) {
+        console.log(
+          `Looks like there was a problem. Status Code: ${response.status}`
+        );
+      } else {
+        // Examine the text in the response
+        return response.json();
+      }
+    })
+    .then((data) => {
+      let dat = data.meals;
+      for (element of dat) {
+        // Create a div with a card class
+        const link = document.createElement("a");
+        link.setAttribute("href", "../selectedProduct/selectedProduct.html");
+        const Meal = document.createElement("div");
+        link.appendChild(Meal);
+        Meal.setAttribute("class", "FirstOpject");
+        Container.appendChild(link);
+        const imgMeal = document.createElement("img");
+        imgMeal.setAttribute("class", "imgOfOpject");
 
+        Meal.appendChild(imgMeal);
+
+        imgMeal.setAttribute("src", `${element.strMealThumb}`);
+        const favoriteDrink = document.createElement("div");
+        favoriteDrink.setAttribute("class", "favorite");
+
+        Meal.appendChild(favoriteDrink);
+
+        const favIcon = document.createElement("i");
+        favIcon.setAttribute("class", "ri-heart-line");
+        favoriteDrink.appendChild(favIcon);
+      }
+    });
 }
+const BackButton = document.querySelector(".BackB");
+BackButton.addEventListener("click", function () {
+  localStorage.removeItem(1);
+});
+const HomeButton = document.querySelector(".Home");
+HomeButton.addEventListener("click", function () {
+  localStorage.removeItem(1);
+});
