60b4369deea8aa38186ab11d78ae4930436ae62b
salsabeelomar <omarsalsabeel65@gmail.com>
2021-08-08 15:56:41 +0300

    in the folder i make the serach input that when the user inters the name of this product it will show it when he prass on search icon , i create the a tag ( anchor) and put the div opject inside the anchor create the function search relates #35

--- drinks/drinks.js
+++ drinks/drinks.js
@@ -1,5 +1,10 @@
+const searchInput = document.getElementsByClassName("searchInput")[0];
+const search = document.getElementsByClassName("SearchB")[0];
+const drinksContainer = document.getElementById("MainContainer");
+
+search.onclick =searchs;
 const api = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s";
-//function to fetch API with try and return error by catch
+// function to fetch API with try and return error by catch
 async function getData() {
   try {
     const response = await fetch(api);
@@ -11,12 +16,12 @@ async function getData() {
 }
 getData();
 
-const drinksContainer = document.getElementById("MainContainer");
-
 // function to create elements and get all photos of drinks from API
 function createCardsOfDrinks(data) {
   let arrayOfDrinks = data.drinks;
   for (let i = 0; i < arrayOfDrinks.length; i++) {
+    const link= document.createElement("a");
+link.setAttribute("href","../selectedProduct/selectedProduct.html")
     const Drink = document.createElement("div");
     Drink.setAttribute("class", "FirstOpject");
     drinksContainer.appendChild(Drink);
@@ -34,5 +39,48 @@ function createCardsOfDrinks(data) {
     const favIcon = document.createElement("i");
     favIcon.setAttribute("class", "ri-heart-line");
     favoriteDrink.appendChild(favIcon);
+    link.appendChild(Drink);
+    drinksContainer.appendChild(link);
   }
+}
+function searchs(e) {
+  e.preventDefault();
+  drinksContainer.textContent = "";
+  fetch(
+    `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchInput.value}`
+  )
+    .then((res) => {
+      if (res.status !== 200) {
+        console.log(`looks like there was an error ${res.status}`);
+      } else {
+        return res.json();
+      }
+    })
+    .then((data) => {
+      for (let i of data.drinks) {
+        const link= document.createElement("a");
+        link.setAttribute("href","../selectedProduct/selectedProduct.html"); 
+
+        const Drink = document.createElement("div");
+        Drink.setAttribute("class", "FirstOpject");
+
+        const imgDrink = document.createElement("img");
+        imgDrink.setAttribute("class", "imgOfOpject");
+        imgDrink.setAttribute("src", `${i.strDrinkThumb}`);
+       
+        const favoriteDrink = document.createElement("div");
+        favoriteDrink.setAttribute("class", "favorite");
+
+        const favIcon = document.createElement("i");
+        favIcon.setAttribute("class", "ri-heart-line");
+
+        Drink.appendChild(imgDrink);;
+        favoriteDrink.appendChild(favIcon);
+        Drink.appendChild(favoriteDrink);
+        link.appendChild(Drink);
+        drinksContainer.appendChild(link);
+      }
+    })
+    .catch((rej) => console.log(rej));
+  searchInput.value = "";
 }
