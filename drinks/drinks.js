50860db52f0c0c1cc846d2af34d1d998d16a471d
salsabeelomar <omarsalsabeel65@gmail.com>
2021-08-10 15:44:42 +0300

    add name for drink and food ang give them style none relates #17

--- drinks/drinks.js
+++ drinks/drinks.js
@@ -1,8 +1,11 @@
 const searchInput = document.getElementsByClassName("searchInput")[0];
 const search = document.getElementsByClassName("SearchB")[0];
 const drinksContainer = document.getElementById("MainContainer");
+const addLocalStorge = [];
 
-search.onclick =searchs;
+search.onclick = searchs;
+drinksContainer.addEventListener("click", forSelected);
+
 const api = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s";
 // function to fetch API with try and return error by catch
 async function getData() {
@@ -17,29 +20,44 @@ async function getData() {
 getData();
 
 // function to create elements and get all photos of drinks from API
+
 function createCardsOfDrinks(data) {
   let arrayOfDrinks = data.drinks;
   for (let i = 0; i < arrayOfDrinks.length; i++) {
-    const link= document.createElement("a");
-link.setAttribute("href","../selectedProduct/selectedProduct.html")
+    // for each element in API Create a tag and give it href attribute that take the user to Selected productpage
+    const link = document.createElement("a");
+    link.setAttribute("class", "item");
+    link.setAttribute("href", "../selectedProduct/selectedProduct.html");
+
+    // create div class FirstOpject inside a(item)
     const Drink = document.createElement("div");
     Drink.setAttribute("class", "FirstOpject");
     drinksContainer.appendChild(Drink);
+
+    //create img tag and give it class imgOfOpject and src attribute and put it inside div(FirstOpject)
     const imgDrink = document.createElement("img");
     imgDrink.setAttribute("class", "imgOfOpject");
-
     Drink.appendChild(imgDrink);
-
     imgDrink.setAttribute("src", `${arrayOfDrinks[i].strDrinkThumb}`);
+
+    // create div tag and give it class favorite and put it inside div            (FirstOpject)
     const favoriteDrink = document.createElement("div");
     favoriteDrink.setAttribute("class", "favorite");
-
     Drink.appendChild(favoriteDrink);
 
+    // create i tag and give it class ri-heart-line and put it inside div(favorite)
     const favIcon = document.createElement("i");
     favIcon.setAttribute("class", "ri-heart-line");
     favoriteDrink.appendChild(favIcon);
     link.appendChild(Drink);
+
+    // create p tag and give it class drinkName,give it taxt from API ng give it  style display as none and put it inside div(FirstOpject)
+    const drinkName = document.createElement("p");
+    drinkName.setAttribute("class", "drinkName");
+    drinkName.textContent = `${arrayOfDrinks[i].strDrink}`;
+    drinkName.style.display = "none";
+    Drink.appendChild(drinkName);
+
     drinksContainer.appendChild(link);
   }
 }
@@ -58,29 +76,50 @@ function searchs(e) {
     })
     .then((data) => {
       for (let i of data.drinks) {
-        const link= document.createElement("a");
-        link.setAttribute("href","../selectedProduct/selectedProduct.html"); 
+        // for each element in API Create a tag and give it href attribute that take the user to Selected productpage
+        const link = document.createElement("a");
+        link.setAttribute("href", "../selectedProduct/selectedProduct.html");
+        link.setAttribute("class", "item");
+        drinksContainer.appendChild(link);
 
+        // create div class FirstOpject inside a(item)
         const Drink = document.createElement("div");
         Drink.setAttribute("class", "FirstOpject");
+        link.appendChild(Drink);
 
+        // create p tag and give it class drinkName,give it taxt from API ng give it  style display as none and put it inside div(FirstOpject)
+        const drinkName = document.createElement("p");
+        drinkName.setAttribute("class", "drinkName");
+        drinkName.textContent = `${i.idDrink}`;
+        drinkName.style.display = "none";
+        Drink.appendChild(drinkName);
+
+        // create div tag and give it class favorite and put it inside div            (FirstOpject)
+        const favoriteDrink = document.createElement("div");
+        favoriteDrink.setAttribute("class", "favorite");
+        Drink.appendChild(favoriteDrink);
+
+        // create i tag and give it class ri-heart-line and put it inside div(favorite)
+        const favIcon = document.createElement("i");
+        favIcon.setAttribute("class", "ri-heart-line");
+        favoriteDrink.appendChild(favIcon);
+
+        //create img tag and give it class imgOfOpject and src attribute and put it inside div(FirstOpject)
         const imgDrink = document.createElement("img");
         imgDrink.setAttribute("class", "imgOfOpject");
         imgDrink.setAttribute("src", `${i.strDrinkThumb}`);
-       
-        const favoriteDrink = document.createElement("div");
-        favoriteDrink.setAttribute("class", "favorite");
-
-        const favIcon = document.createElement("i");
-        favIcon.setAttribute("class", "ri-heart-line");
-
-        Drink.appendChild(imgDrink);;
-        favoriteDrink.appendChild(favIcon);
-        Drink.appendChild(favoriteDrink);
-        link.appendChild(Drink);
-        drinksContainer.appendChild(link);
+        Drink.appendChild(imgDrink);
       }
     })
     .catch((rej) => console.log(rej));
+
   searchInput.value = "";
+}
+// This function make repalce the value for each click on dish or drink and save in localStorge ,this is the same function in drink page .
+function forSelected(items) {
+  const select = items.target.parentElement;
+  const selectedElemet =
+    select.getElementsByClassName("drinkName")[0].innerHTML;
+  const s = localStorage.setItem("value", selectedElemet);
+  addLocalStorge.push(s);
 }
